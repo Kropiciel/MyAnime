a14f702494d05a61151d8b38ec094c002d70bcab
Kropiciel <tomcej9@gmail.com>
2021-08-23 18:10:47 +0200

    Changed .get_all_scores() and added new method .compare_with(friend_name)

--- MyAnime.py
+++ MyAnime.py
@@ -6,44 +6,55 @@ import requests
 import bs4
 import random
 from typing import Optional
+import pandas as pd
+from collections import namedtuple
+
+pd.set_option("display.max_rows", 999)
+pd.set_option("display.max_columns", 5)
+pd.set_option('display.max_colwidth', 150)
+pd.set_option("expand_frame_repr", True)
 
 
 class MyAnime:
     """A class to manipulate data from my anime list."""
 
     _STATUS_ALL = 7
+    _STATUS_DICT = {'1': 'Currently Watching', '2': 'Completed', '3': 'On Hold', '4': 'Dropped', '6': 'Plan to Watch'}
 
     def __init__(self, nickname) -> None:
         """Initialize self.
 
         :param nickname: a nickname of the user to fetch the list for
         """
+        self.anime_details_all = []
         self.nickname = nickname
 
-    def get_all_scores(self) -> dict[str, int]:
-        """Get the user score map
+    Anime = namedtuple('Anime', 'title, score, status')
+
+    def get_all_scores(self) -> list[Anime]:
+        """Get the user score and status map
 
         Makes a request to MyAnimeList to download the user list.
 
-        :return: a dictionary of anime title to anime score
+        :return: a list of tuples of anime title, anime score and anime status
         """
         json_request = 0
-        all_titles_scores = {}
         while True:
             req = requests.get(
-                f'https://myanimelist.net/animelist/{self.nickname}/load.json?offset={json_request}&status={self._STATUS_ALL}')
+                f'https://myanimelist.net/animelist/{self.nickname}/'
+                f'load.json?offset={json_request}&status={self._STATUS_ALL}')
             soup = bs4.BeautifulSoup(req.text, "lxml")
             select_anime_list = soup.select('p')[0]
             json_format = select_anime_list.getText()
             anime_details = json.loads(json_format)
-            anime_score = {anime['anime_title']: anime['score'] for anime in anime_details}
-            all_titles_scores = {**all_titles_scores, **anime_score}
+            self.anime_details_all += anime_details
 
-            if len(anime_details) < 100:
+            if len(anime_details) < 300:
                 break
             json_request += 300
 
-        return all_titles_scores
+        return [self.Anime(anime['anime_title'], anime['score'], self._STATUS_DICT[str(anime['status'])])
+                for anime in self.anime_details_all]
 
     def get_score_for_title(self, title) -> Optional[int]:
         """Retrieve score for a given title.
@@ -56,7 +67,7 @@ class MyAnime:
         """
         title_score = self.get_all_scores()
 
-        for item_title, item_score in title_score.items():
+        for item_title, item_score, item_status in title_score:
             if item_title.lower() == title.lower():
                 return item_score
 
@@ -67,5 +78,40 @@ class MyAnime:
 
         :return: anime title
         """
-        titles = list(self.get_all_scores().keys())
+        titles = [title for title, score, status in self.get_all_scores()]
         return random.choice(titles)
+
+    def compare_with(self, friend_nickname):
+        """Compare user score and anime status with another user for anime title that both have in their lists.
+
+        :param friend_nickname: a nickname of another user to fetch the list for
+
+        :return: A table with a common anime title, both users' scores and anime status
+        """
+        friend = MyAnime(friend_nickname)
+
+        my_all_scores = self.get_all_scores()
+        friend_all_scores = friend.get_all_scores()
+
+        my_all_titles_set = set([title for title, score, status in my_all_scores])
+        friend_all_titles_set = set([title for title, score, status in friend_all_scores])
+
+        mutual_anime = my_all_titles_set.intersection(friend_all_titles_set)
+
+        my_mutual_anime_scores = {title: score for title, score, status in my_all_scores if title in mutual_anime}
+        friend_mutual_anime_scores = {title: score for title, score, status in my_all_scores if title in mutual_anime}
+
+        my_anime_status = {title: status for title, score, status in my_all_scores}
+        friend_anime_status = {title: status for title, score, status in friend_all_scores}
+
+        compared = sorted([(anime, my_mutual_anime_scores[anime], my_anime_status[anime],
+                            friend_mutual_anime_scores[anime], friend_anime_status[anime]) for anime in mutual_anime])
+
+        table = pd.DataFrame(compared, columns=['Anime Title', self.nickname, self.nickname + ' anime status',
+                                                friend_nickname, friend_nickname + ' anime status'])
+        table.index += 1
+
+        return table
+
+ja = MyAnime('Kropiciel')
+print(ja.compare_with('feyaxa'))
